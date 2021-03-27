45be649351b7dd9030ba6f429000a524dc2a0240
BryceHamilton <bryce.m.hamilton@outlook.com>
2021-03-26 23:29:11 -0400

    add morgan and sample .env

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 express()
+  .use(morgan('dev'))
   .use(express.urlencoded({ extended: false }))
   .use(express.json())
 
