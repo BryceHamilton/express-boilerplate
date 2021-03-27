65ba28f21cf3b277bfd823ea825acab9fd41df0d
BryceHamilton <bryce.m.hamilton@outlook.com>
2021-03-26 23:56:43 -0400

    add mongoose

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.DB_URL || '', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('db connected'));
 
 express()
   .use(morgan('dev'))
